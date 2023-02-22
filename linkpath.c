f4a32301fff7e38c80876083d82a93120df07036
motirase <117908555+motirase@users.noreply.github.com>
2023-02-22 09:47:15 +0300

    Update linkpath.c

--- linkpath.c
+++ linkpath.c
@@ -1,148 +1,65 @@
 #include "shell.h"
-
-
 /**
- * _getenv - gets the value of the global variable
- * @name: name of the global variable
- * Return: string of value
- */
-char *_getenv(const char *name)
-{
-	int i, j;
-	char *value;
+ * path_check - finds correct path based on input
+ * @head: head of the path list
+ * @input: arguments passed
+ * Return: NULL if not matched
+*/
 
-	if (!name)
+char *path_check(path_t *head, char *line)
+{
+
+/*	struct stat falcon;*/
+	path_t *temp;
+	char *arguments;
+
+	if (!line || !head)
 		return (NULL);
-	for (i = 0; environ[i]; i++)
+
+	temp = head;
+	while (temp)
 	{
-		j = 0;
-		if (name[j] == environ[i][j])
+		arguments = path_cat(temp->ptr, line);
+		if (!arguments)
+			return (NULL);
+/*		if (stat(arguments, &falcon) == 0)
 		{
-			while (name[j])
-			{
-				if (name[j] != environ[i][j])
-					break;
-
-				j++;
-			}
-			if (name[j] == '\0')
-			{
-				value = (environ[i] + j + 1);
-				return (value);
-			}
-		}
+			return (arguments);
+		}*/
+		free(arguments);
+		temp = temp->next;
 	}
-	return (0);
-}
-
-
-/**
- * add_node_end - adds a new node at the end of a list_t list
- * @head: pointer to pointer to our linked list
- * @str: pointer to string in previous first node
- * Return: address of the new element/node
- */
-
-list_path *add_node_end(list_path **head, char *str)
-{
-
-	list_path *tmp;
-	list_path *new;
-
-	new = malloc(sizeof(list_path));
-
-	if (!new || !str)
-	{
-		return (NULL);
-	}
-
-	new->dir = str;
-
-	new->p = '\0';
-	if (!*head)
-	{
-		*head = new;
-	}
-	else
-	{
-		tmp = *head;
-
-		while (tmp->p)
-		{
-
-			tmp = tmp->p;
-		}
-
-		tmp->p = new;
-	}
-
-	return (*head);
-}
-
-
-/**
- * linkpath - creates a linked list for path directories
- * @path: string of path value
- * Return: pointer to the created linked list
- */
-list_path *linkpath(char *path)
-{
-	list_path *head = '\0';
-	char *token;
-	char *cpath = _strdup(path);
-
-	token = strtok(cpath, ":");
-	while (token)
-	{
-		head = add_node_end(&head, token);
-		token = strtok(NULL, ":");
-	}
-
-	return (head);
-}
-
-/**
- * _which - finds the pathname of a filename
- * @filename: name of file or command
- * @head: head of linked list of path directories
- * Return: pathname of filename or NULL if no match
- */
-char *_which(char *filename, list_path *head)
-{
-	struct stat st;
-	char *string;
-
-	list_path *tmp = head;
-
-	while (tmp)
-	{
-
-		string = concat_all(tmp->dir, "/", filename);
-		if (stat(string, &st) == 0)
-		{
-			return (string);
-		}
-		free(string);
-		tmp = tmp->p;
-	}
-
 	return (NULL);
 }
 
 /**
- * free_list - frees a list_t
- *@head: pointer to our linked list
+ * path_cat - concats the commands to the PATH
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to the concated string
  */
-void free_list(list_path *head)
+char *path_cat(char *s1, char *s2)
 {
-	list_path *storage;
+	char *together;
+	int count1, count2 = 0;
 
-	while (head)
-	{
-		storage = head->p;
-		free(head->dir);
-		free(head);
-		head = storage;
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
+	together = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 2));
+	if (together == NULL)
+		return (NULL);
+	for (count1 = 0; s1[count1]; count1++)
+		together[count1] = s2[count1];
+
+	together[count1] = '/';
+	count1++;
+
+	for (count2 = 0; s2[count2]; count2++)
+		together[count1 + count2] = s2[count2];
+	together[count1 + count2] = '\0';
+	printf("aaa%s\n", together);
+	return (together);
 }
